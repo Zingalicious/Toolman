50ddf2f5a1cec1137c41f0ae34ab2f985bd18718
zingten11 <admin@zingalicio.us>
2014-07-28 13:21:44 -0700

    Added null check on extra itemstack amount thing yeah

--- src/main/java/us/zingalicio/toolman/tools/Replicator.java
+++ src/main/java/us/zingalicio/toolman/tools/Replicator.java
@@ -15,97 +15,114 @@ import us.zingalicio.zinglib.util.NameUtil;
 
 public class Replicator extends AbstractTool
 {
-  Toolman plugin;
+	Toolman plugin;
 
-  public Replicator(Toolman plugin)
-  {
-    this.plugin = plugin;
-    this.toolName = "Replicator";
-  }
+	public Replicator(Toolman plugin)
+	{
+		this.plugin = plugin;
+		this.toolName = "Replicator";
+	}
 
-  @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 public void onRangedUse(Player player, ItemStack item, Action action)
-  {
-    PlayerInventory inventory = player.getInventory();
+	{
+		PlayerInventory inventory = player.getInventory();
 
-    if ((action == Action.LEFT_CLICK_AIR) || (action == Action.LEFT_CLICK_BLOCK))
-    {
-      for (int i = 0; i < 9; i++)
-      {
-        ItemStack invItem = inventory.getItem(i);
-        if (invItem != null)
-        {
-          int maxStackSize = invItem.getMaxStackSize();
+		if ((action == Action.LEFT_CLICK_AIR) || (action == Action.LEFT_CLICK_BLOCK))
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				ItemStack invItem = inventory.getItem(i);
+				if (invItem != null)
+				{
+					int maxStackSize = invItem.getMaxStackSize();
 
-          if ((invItem.getAmount() != maxStackSize) && (invItem.getType() != Material.EMERALD) && (invItem.getData().getData() != 1))
-          {
-            invItem.setAmount(maxStackSize);
-          }
-        }
-      }
-      MessageUtil.sendMessage(plugin, player, "Refilled hotbar.");
-      player.getWorld().playSound(player.getLocation(), Sound.LAVA_POP, 0.1F, 0.031F);
-      player.updateInventory();
-      return;
-    }
+					if ((invItem.getAmount() != maxStackSize) && (invItem.getType() != Material.EMERALD) && (invItem.getData().getData() != 1))
+					{
+						invItem.setAmount(maxStackSize);
+					}
+				}
+			}
+			MessageUtil.sendMessage(plugin, player, "Refilled hotbar.");
+			player.getWorld().playSound(player.getLocation(), Sound.LAVA_POP, 0.1F, 0.031F);
+			player.updateInventory();
+			return;
+		}
 
-    if (player.isSneaking())
-    {
-      Block targetBlock = player.getTargetBlock(null, this.plugin.getToolManager().getRange());
+		else if (player.isSneaking())
+		{
+			Block targetBlock = player.getTargetBlock(null, this.plugin.getToolManager().getRange());
 
-      Material mat = targetBlock.getType();
-      byte data = targetBlock.getData();
+			Material mat = targetBlock.getType();
+			byte data = targetBlock.getData();
 
-      int maxStackSize = mat.getMaxStackSize();
+			int maxStackSize = mat.getMaxStackSize();
 
-      ItemStack newStack = new ItemStack(mat, maxStackSize, data);
+			ItemStack newStack = new ItemStack(mat, maxStackSize, data);
 
-      ItemStack extra = inventory.addItem(newStack).get(0);
-      int amt = maxStackSize - extra.getAmount();
-      MessageUtil.sendMessage(plugin, player, "Given " + amt + " " + NameUtil.getFullName(plugin, newStack.getType(), newStack.getData()) + ".");
-      player.getWorld().playSound(player.getLocation(), Sound.LAVA_POP, 0.1F, 0.031F);
-      player.updateInventory();
-    }
-  }
+			ItemStack extra = inventory.addItem(newStack).get(0);
+			int amt;
+			if(extra != null)
+			{
+				amt = maxStackSize - extra.getAmount();
+			}
+			else
+			{
+				amt = maxStackSize;
+			}
+			MessageUtil.sendMessage(plugin, player, "Given " + amt + " " + NameUtil.getFullName(plugin, newStack.getType(), newStack.getData()) + ".");
+			player.getWorld().playSound(player.getLocation(), Sound.LAVA_POP, 0.1F, 0.031F);
+			player.updateInventory();
+		}
+	}
 
-  @SuppressWarnings("deprecation")
+	@SuppressWarnings("deprecation")
 public void onCloseUse(Block block, BlockFace blockFace, Player player, ItemStack item, Action action)
-  {
-    PlayerInventory inventory = player.getInventory();
+	{
+		PlayerInventory inventory = player.getInventory();
 
-    if ((action == Action.LEFT_CLICK_AIR) || (action == Action.LEFT_CLICK_BLOCK))
-    {
-      for (int i = 0; i < 9; i++)
-      {
-        ItemStack invItem = inventory.getItem(i);
-        if (invItem != null)
-        {
-          int maxStackSize = invItem.getMaxStackSize();
+		if ((action == Action.LEFT_CLICK_AIR) || (action == Action.LEFT_CLICK_BLOCK))
+		{
+			for (int i = 0; i < 9; i++)
+			{
+				ItemStack invItem = inventory.getItem(i);
+				if (invItem != null)
+				{
+					int maxStackSize = invItem.getMaxStackSize();
 
-          if ((invItem.getAmount() != maxStackSize) && (invItem.getType() != Material.EMERALD) && (invItem.getData().getData() != 1))
-          {
-            invItem.setAmount(maxStackSize);
-          }
-        }
-      }
-      MessageUtil.sendMessage(plugin, player, "Refilled hotbar.");
-      player.getWorld().playSound(player.getLocation(), Sound.LAVA_POP, 0.1F, 0.031F);
-      player.updateInventory();
-      return;
-    }
-
-    Material mat = block.getType();
-    byte data = block.getData();
-
-    int maxStackSize = mat.getMaxStackSize();
-
-    ItemStack newStack = new ItemStack(mat, maxStackSize, data);
-
-    ItemStack extra = inventory.addItem(newStack).get(0);
-    int amt = maxStackSize - extra.getAmount();
-    MessageUtil.sendMessage(plugin, player, "Given " + amt + " " + NameUtil.getFullName(plugin, newStack.getType(), newStack.getData()) + ".");
-
-    player.getWorld().playSound(player.getLocation(), Sound.LAVA_POP, 0.1F, 0.031F);
-    player.updateInventory();
-  }
+					if ((invItem.getAmount() != maxStackSize) && (invItem.getType() != Material.EMERALD) && (invItem.getData().getData() != 1))
+					{
+						invItem.setAmount(maxStackSize);
+					}
+				}
+			}
+			MessageUtil.sendMessage(plugin, player, "Refilled hotbar.");
+			player.getWorld().playSound(player.getLocation(), Sound.LAVA_POP, 0.1F, 0.031F);
+			player.updateInventory();
+			return;
+		}
+		else
+		{
+			Material mat = block.getType();
+			byte data = block.getData();
+	
+			int maxStackSize = mat.getMaxStackSize();
+	
+			ItemStack newStack = new ItemStack(mat, maxStackSize, data);
+			ItemStack extra = inventory.addItem(newStack).get(0);
+			int amt;
+			if(extra != null)
+			{
+				amt = maxStackSize - extra.getAmount();
+			}
+			else
+			{
+				amt = maxStackSize;
+			}
+			MessageUtil.sendMessage(plugin, player, "Given " + amt + " " + NameUtil.getFullName(plugin, newStack.getType(), newStack.getData()) + ".");
+	
+			player.getWorld().playSound(player.getLocation(), Sound.LAVA_POP, 0.1F, 0.031F);
+			player.updateInventory();
+		}
+	}
 }
